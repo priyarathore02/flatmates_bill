0440e294c9b01b0075bf83474cb0df94341474c2
Priya <pr302209@gmai.com>
2024-08-01 14:34:55 +0530

    write code for asking details from users.

--- main.py
+++ main.py
@@ -60,15 +60,23 @@ class PdfReport():
 
         webbrowser.open(self.filename)
 
-the_bill = Bill(amount=120,period="July 2024")
-john = Flatmate(name="john", days_in_house=20)
-marry = Flatmate(name="marry", days_in_house=25)
+amount = float(input("Enter amount: "))
+period = input("Enter period: ")
+name1 = input("Enter first flatmate name: ")
+days_in_house1 = int(input(f"Enter number of days did {name1} stayed in house: "))
+name2 = input("Enter second flatmate name: ")
+days_in_house2 = int(input(f"Enter number of days did {name2} stayed in house: "))
 
-print("john pays: ",john.pays(bill=the_bill,other_flatmate= marry))
-print("marry pays:",marry.pays(bill=the_bill,other_flatmate= john))
 
-pdf_report = PdfReport(filename= "flatmates.pdf")
-pdf_report.generate_pdf(flatmate1= john,flatmate2= marry,bill= the_bill)
+the_bill = Bill(amount,period)
+flatmate1 = Flatmate(name1, days_in_house1)
+flatmate2 = Flatmate(name2, days_in_house2)
+
+print(f"{name1} pays: ",flatmate1.pays(the_bill,flatmate2))
+print(f"{name2} pays:",flatmate2.pays(the_bill,flatmate1))
+
+pdf_report = PdfReport(f"{the_bill.period}.pdf")
+pdf_report.generate_pdf(flatmate1,flatmate2 , the_bill)
 
 
 
