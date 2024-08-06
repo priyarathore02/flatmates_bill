3aa8170b38be80f0ebc4a19cb3fdf98b59ed8971
Priya <pr302209@gmai.com>
2024-08-06 16:39:33 +0530

    debug is removed from run function.

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 from flask.views import MethodView
 from wtforms import Form, StringField, SubmitField
-from flask import Flask , render_template
-
-from wtforms.validators import DataRequired
+from flask import Flask , render_template, request
+from flatmates_bill import flat
 
 app=Flask(__name__)
 
@@ -16,7 +15,19 @@ class BillFormPage(MethodView):
         return render_template("bill_form_page.html", billform=bill_form)
 
 class ResultsPage(MethodView):
-    pass
+    def post(selfself):
+        billform = BillForm(request.form)
+
+        the_bill= flat.Bill(float(billform.amount.data), billform.period.data)
+        flatmate1= flat.Flatmate(billform.name1.data,float(billform.days_in_house1.data))
+        flatmate2 = flat.Flatmate(billform.name2.data, float(billform.days_in_house2.data))
+
+        return render_template("results.html",
+                               name1=flatmate1.name,
+                               amount1= round(flatmate1.pays(the_bill,flatmate2)),
+                               name2=flatmate2.name,
+                               amount2= round(flatmate2.pays(the_bill,flatmate1)))
+
 class BillForm(Form):
     amount = StringField('Bill Amount: ')
     period = StringField('Bill Period: ')
@@ -32,5 +43,6 @@ class BillForm(Form):
 
 app.add_url_rule('/',view_func=HomePage.as_view('home_page'))
 app.add_url_rule('/bill',view_func=BillFormPage.as_view('bill_form_page'))
+app.add_url_rule("/results",view_func=ResultsPage.as_view('results_page'))
 
-app.run(debug=True)
+app.run()
